75e6854106505b964e12a6183e332d7ca3104312
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-07-14 11:05:08 -0700

    fix profile

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/Navbar";
 // import { GeistMono } from 'geist/font/mono';
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -34,7 +34,7 @@ export default function RootLayout({
 					<Navbar />
 					{children}
 				</>
-         <Toaster />
+				<Toaster />
 			</body>
 		</html>
 	);
